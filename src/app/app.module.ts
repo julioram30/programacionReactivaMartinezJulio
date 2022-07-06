c00229aacb44d194b2814be808d2fdf954b7f535
julioram30 <julioram30@yahoo.com.ar>
2022-07-05 23:37:32 -0300

    con error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsumidorComponent } from './components/consumidor/consumidor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ConsumidorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
